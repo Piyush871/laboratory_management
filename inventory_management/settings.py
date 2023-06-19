fb41d16bb7829a02be567b34a8ea1f3741071d97
piyush jaiswal <jgff19852@gmail.com>
2023-06-19 13:07:40 +0530

    last commit

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -143,7 +143,6 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
-SITE_DOMAIN = 'localhost:8000'
 
 DEBUG = True
 # For django.contrib.sites
